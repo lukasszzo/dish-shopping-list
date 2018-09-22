d3cc107226600c05fd475a2173ea255702d1daf8
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-09-22 11:05:17 +0200

    created shopping list with form,add edit section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[]=[
+    new Ingredient('Tomatoes',12),
+    new Ingredient('Meat',2),
+
+  ];
 
   constructor() { }
 
