c16f1ae32537fbc4fc02888178158625164759da
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-09-22 10:39:02 +0200

    built structure view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('A test recipe','this siple test','https://www.winiary.pl/image.ashx/kaszanka-z-cebula-i-czosnkiem.jpg?fileID=192132&width=800&height=1400&frame=False&bg=0&resize=1&crop=0&hRefill=0&vRefill=0&quality=84'),
+    new Recipe('Schabowy','this siple test','https://upload.wikimedia.org/wikipedia/commons/a/ac/Kotlet_schabowy_56.jpg')
+  ];
 
   constructor() { }
 
